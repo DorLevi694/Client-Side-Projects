d64c7780de48f09fbe396d6f1cfc5ea856630b9d
Dor <DorLevi694@gmail.com>
2021-06-04 04:51:54 +0300

    little change in css .. not done

--- pop-quiz - fully reactive app/src/app/models/questions.ts
+++ pop-quiz - fully reactive app/src/app/models/questions.ts
@@ -1,48 +1,51 @@
 
 import { Question } from './question';
 
-export let questionList: Question[] =
-    [
-        {
-            caption: 'Q11111111111111',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q2222222222',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q333333333333',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q444444444444444',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q555555555555',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q6666666666666',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        },
-        {
-            caption: 'Q777777777777',
-            answers: ["ans1", "ans2", "ans3", "ans4"],
-            correctAnswer: 1,
-            userAnswer: -1
-        }
-    ];
+export let questionList: Question[] = [
+    {
+        caption: "Which internet company began life as an online bookstore calledCadabra?",
+        answers: ["Amazon", "eBay", "Overstock", "Shopify"],
+        correctAnswer: 1,
+        userAnswer: -1
+    },
+
+    {
+        caption: "Nvidia headquarters are based in which Silicon Valley city?",
+        answers: ["Palo Alto","Santa Clara",  "Cupertino", "Mountain View"],
+        correctAnswer: 2,
+        userAnswer: -1
+    },
+    {
+        caption: "Which of these is the name for the failed key escrow device introduced by the National Security Agency in 1993?",
+        answers: [ "Enigma Machine", "Skipjack", "Nautilus","Clipper Chip"],
+        correctAnswer: 4,
+        userAnswer: -1
+    },
+    {
+        caption: "Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+        answers: ["JavaScript", "C#", "Python", "PHP"],
+        correctAnswer: 1,
+        userAnswer: -1
+    },
+    {
+        caption: "In programming, the ternary operator is mostly defined with what symbol(s)?",
+        answers: [ "??", "if then","?:", "?"],
+        correctAnswer: 3,
+        userAnswer: -1
+
+    },
+    {
+        caption: "How fast is USB 3.1 Gen 2 theoretically?",
+        answers: [ "5 Gbs", "10 Gbs","8 Gbs", "1 Gbs"],
+        correctAnswer: 2,
+        userAnswer: -1
+
+    },
+    {
+        caption: ".at is the top-level domain for what country?",
+        answers: [ "Argentina", "Australia", "Austria","Angola"],
+        correctAnswer: 3,
+        userAnswer: -1
+    },
+
+];
